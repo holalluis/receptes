2a91177a226821e1fac049c34d9800e6abd2676a
lbosch <lbosch@icra.cat>
2017-04-17 19:47:26 +0200

    logo fons

--- brunyolsThermo.php
+++ brunyolsThermo.php
@@ -1,4 +1,3 @@
-
 <?php include'navbar.php'?>
 
 <h3>BRUNYOLS / thermomix</h3>
@@ -8,26 +7,34 @@
 <ul>
 	<li>650 g farina de força
 	<li>150 g sucre
-	<li>65 g llard de porc (també val mantega, però el resultat no és el mateix!)
-	<li>35 g llevat premsat
-	<li>8 g de sal
+	<li>65  g llard de porc (també val mantega però el resultat no és el mateix!)
+	<li>35  g llevat premsat
+	<li>8   g de sal
 	<li>200 ml llet sencera
-	<li>10 ml aigua naf
-	<li>15 ml anís líquid
-	<li>15 g matafaluga, celiandre (pot ser en pols o en gra)
-	<li>4 ous
-	<li>12 g de pell de llimona
+	<li>10  ml aigua naf
+	<li>15  ml anís líquid
+	<li>15  g matafaluga, celiandre (pot ser en pols o en gra)
+	<li>4   ous
+	<li>12  g de pell de llimona
 	<li>Oli d'oliva
 
-	<li>Instruccions:
-		<ul>
-			<li>Preparar la massa:
-			<li>1. Introduir en el got la llard i la llevadura , 30 segons  vel 4 temp 37 graus.
-			<li>2. Afegir la resta dels ingredients , excepte els ous . Amasar   . Anar afegint els ous un a un. No incorporar l’altre fins que hagi queden ben barrejat. 6 minuts , aprox  fins que la massa quedi lligada i una textura fina i  molt elàstica.
-			<li>3. Tapar amb un drap i deixar reposar en un lloc tebi i sense corrent Esperarem dues o tres hores, i teòricament la massa doblarà el seu volum.
-			<li>4. Escalfar l’ oli d'oliva en un cassó, per a que quedi un bon espai entre la superfície i la base del cassó. Encara que a la recepta digui que ha de fumejar, feu-me cas, no espereu a que estigui tan calent perquè quedarien negres per fora.
-			<li>5. Ficar el dit índex al mig de la boleta fins a fer-hi un forat, i alçar el brunyol per a abocar-lo a l'oli (això ho veureu molt i molt clar al vídeo). Els girarem una sola vegada, quan comencin a daurar-se per sota, i els traurem quan adquireixin un color daurat fosc.
-			<li>6. Ensucrem mentre siguin calents. Deixem reposar per a que tinguin un gust
+	<li>Instruccions preparar la massa:
+		<ul style=list-style-type:decimal>
+			<li>Introduir en el got la llard i el llevat, 30 segons, vel 4, a 37 ºC.
+			<li>Afegir la resta d'ingredients, excepte els ous. 
+			<li>Amassar. 
+			<li>Anar afegint els ous un a un. 
+			<li>No incorporar l’altre fins que hagi queden ben barrejat. 6 minuts aprox. 
+				fins que la massa quedi lligada i una textura fina i  molt elàstica.
+			<li>Tapar amb un drap i deixar reposar en un lloc tebi i sense corrent.
+			<li>Esperarem dues o tres hores, i teòricament la massa doblarà el seu volum.
+			<li>Escalfar l’oli d'oliva en un cassó, perquè quedi un bon espai entre la superfície i la base del cassó. 
+				Encara que a la recepta digui que ha de fumejar, feu-me cas, no espereu a que estigui tan calent perquè quedarien negres per fora.
+			<li>Ficar el dit índex al mig de la boleta fins a fer-hi un forat, 
+				i alçar el brunyol per a abocar-lo a l'oli. 
+				Els girarem una sola vegada, quan comencin a daurar-se per sota, 
+				i els traurem quan adquireixin un color daurat fosc.
+			<li>Ensucrem mentre siguin calents.
 		</ul>
 </ul>
 
--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
-<!doctype html-->
-<html lang=ca>
+<!doctype html>
+<html>
 	<head>
 		<meta charset=utf-8>
 		<meta name=viewport content="width=device-width, initial-scale=1.0">
@@ -7,18 +7,27 @@
 		<style>
 			body{
 				max-width:50em;
-				padding:1em 0.5em;
 				margin:0 auto;
 				background:#fdfdfa;
 			}
 			a{
 				color:#00b7c6;
 			}
+			h2{
+				margin:0;
+				background:#00b7c6;
+				padding:10px;
+			}
+			ul{
+				padding-left:20px;
+			}
 		</style>
 	</head>
 <body>
 
 <h2>
-	<a href=index.php style="color:black;text-decoration:none">Les receptes de la iaia Roser</a>
+	<a href=index.php style="color:black;text-decoration:none">
+		Les receptes de la iaia Roser</a>
 	<span>&#128117;</span> 
 </h2>
+
