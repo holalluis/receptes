73da2d2fd981d99af843b628c67ab6dae93cf44e
Lluís Bosch <holalluis@gmail.com>
2023-02-11 22:08:20 +0100

    update

--- escudella.php
+++ escudella.php
@@ -2,24 +2,26 @@
 <h3>Escudella i carn d'olla</h3>
 <div>Per 4 persones. Posar a bullir:</div>
 <ul>
-  <li>Conill de vedella
-  <li>Ós pelat de vedella
-  <li>Carcassa de pollastre (i si en tens, coll i pedrer)
-  <li>Ós d'espinada (de tocino)
-  <li>Peu de porc
-
-  <li>Col (una meitat o una quarta part)
-  <li>Una ceba
-  <li>Api (2 troncs de fulla)
-  <li>Un porro
-
-  <li>Pastanaga
-  <li>Moniato (opcional)
-  <li>Nap (opcional)
-  <li>Mongetes del ganxet i/o cigrons (opcional)
-
-  <li>Patata (posar al final perquè no es desfaci)
-  <li>Butifarra negra (si t'hi agrada)
+  <li>
+    Posar en una olla a bullir:
+    <ul>
+      <li>Conill de vedella
+      <li>Ós pelat de vedella
+      <li>Carcassa de pollastre (i si en tens, coll i pedrer)
+      <li>Ós d'espinada (de tocino)
+      <li>Peu de porc
+      <li>Col (una meitat o una quarta part)
+      <li>Una ceba
+      <li>Api (2 troncs de fulla)
+      <li>Un porro
+      <li>Pastanaga
+      <li>Moniato (opcional)
+      <li>Nap (opcional)
+      <li>Mongetes del ganxet i/o cigrons (opcional)
+      <li>Patata (posar al final perquè no es desfaci)
+      <li>Butifarra negra (si t'hi agrada)
+    </ul>
+  </li>
 
   <li>Per fer la pilota, barrejar en un plat fondo:
     <ul>
@@ -31,5 +33,9 @@
     Donar-li la forma de pilota, enfarinar i posar dins l'olla.
   </li>
 
-  <li>Colar el caldo, i fer arròs i fideus a part (una tassa d'arròs i 2 de fideus)
+  <li>
+    Colar el caldo, i posar-hi arròs i fideus (una tassa d'arròs i 2 de fideus)
+    durant uns 15-20 minuts
+  </li>
 </ul>
+
