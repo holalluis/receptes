45c48d36dff22af3bc927e244fc3b9decf1bfd59
Lluís Bosch <holalluis@gmail.com>
2023-02-11 21:50:24 +0100

    update

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include'navbar.php'?>
-
 <h3>Index</h3>
-
 <ul>
 	<li><a href=brownie.php>Brownie</a>
 	<li><a href=cremaCatalana.php>Crema catalana</a>
@@ -9,4 +7,5 @@
 	<li><a href=pastisFormatge.php>Pastís de formatge</a>
 	<li><a href=brunyolsThermo.php>Brunyols (thermomix)</a>
   <li><a href="granissat-de-llimona.php">Granissat de llimona</a>
+  <li><a href="escudella.php">Escudella i carn d'olla</a>
 </ul>
